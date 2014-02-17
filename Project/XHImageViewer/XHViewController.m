7d5512521ed0849bdaaca5006ae1bfceb3fc583b
xhzengAIB <xhzengAIB@gmail.com>
2014-02-17 15:43:24 +0000

    Add example ViewController

---
+++ Project/XHImageViewer/XHViewController.m
@@ -0,0 +1,39 @@
+//
+//  XHViewController.m
+//  XHImageViewer
+//
+//  Created by 曾 宪华 on 14-2-17.
+//  Copyright (c) 2014年 曾宪华 开发团队(http://iyilunba.com ) 本人QQ:543413507 本人QQ群（142557668）. All rights reserved.
+//
+
+#import "XHViewController.h"
+
+@interface XHViewController ()
+
+@end
+
+@implementation XHViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+    
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
