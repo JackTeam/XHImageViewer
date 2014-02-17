8e30de0435cdd92174bc7b8b561e05179ae77ae8
xhzengAIB <xhzengAIB@gmail.com>
2014-02-17 14:57:34 +0000

    Add XHImageViewer class

---
+++ Source/XHImageViewer.m
@@ -0,0 +1,31 @@
+//
+//  XHImageViewer.m
+//  XHImageViewer
+//
+//  Created by 曾 宪华 on 14-2-17.
+//  Copyright (c) 2014年 曾宪华 开发团队(http://iyilunba.com ) 本人QQ:543413507 本人QQ群（142557668）. All rights reserved.
+//
+
+#import "XHImageViewer.h"
+
+@implementation XHImageViewer
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
