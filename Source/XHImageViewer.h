fe7ad210b0de6c9a0a9b05b518815aec32bd2f25
xhzengAIB <xhzengAIB@gmail.com>
2014-02-17 15:13:47 +0000

    Design XHImageViewer api

--- Source/XHImageViewer.h
+++ Source/XHImageViewer.h
@@ -7,7 +7,18 @@
 //
 
 #import <UIKit/UIKit.h>
+@class XHImageViewer;
+@protocol XHImageViewerDelegate <NSObject>
+
+@optional
+- (void)imageViewer:(XHImageViewer *)imageViewer  willDismissWithSelectedView:(UIImageView*)selectedView;
+
+@end
 
 @interface XHImageViewer : UIView
 
+@property (nonatomic, weak) id<XHImageViewerDelegate> delegate;
+@property (nonatomic, assign) CGFloat backgroundScale;
+
+- (void)showWithImageViews:(NSArray*)views selectedView:(UIImageView*)selectedView;
 @end
