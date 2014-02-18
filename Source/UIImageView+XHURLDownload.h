3a96ecd0622180e83f5483b2f0a7128cc6b0c75b
xhzengAIB <xhzengAIB@gmail.com>
2014-02-18 10:52:13 +0000

    Design UIImageView (XHURLDownload) api

--- Source/UIImageView+XHURLDownload.h
+++ Source/UIImageView+XHURLDownload.h
@@ -8,6 +8,39 @@
 
 #import <UIKit/UIKit.h>
 
+typedef NS_ENUM(NSInteger, UIImageViewURLDownloadState) {
+    UIImageViewURLDownloadStateUnknown = 0,
+    UIImageViewURLDownloadStateLoaded,
+    UIImageViewURLDownloadStateWaitingForLoad,
+    UIImageViewURLDownloadStateNowLoading,
+    UIImageViewURLDownloadStateFailed,
+};
+
 @interface UIImageView (XHURLDownload)
+
+// url
+@property (nonatomic, strong) NSURL *url;
+
+// download state
+@property (nonatomic, assign) UIImageViewURLDownloadState state;
+
+// UI
+@property (nonatomic, strong) UIView *loadingView;
+// Set UIActivityIndicatorView as loadingView
+- (void)setDefaultLoadingView;
+
+// instancetype
++ (id)imageViewWithURL:(NSURL*)url autoLoading:(BOOL)autoLoading;
+
+// Get instance that has UIActivityIndicatorView as loadingView by default
++ (id)indicatorImageView;
++ (id)indicatorImageViewWithURL:(NSURL*)url autoLoading:(BOOL)autoLoading;
+
+// Download
+- (void)setUrl:(NSURL *)url autoLoading:(BOOL)autoLoading;
+- (void)load;
+- (void)loadWithURL:(NSURL *)url;
+- (void)loadWithURL:(NSURL*)url completionBlock:(void(^)(UIImage *image, NSURL *url, NSError *error))handler;
+
 
 @end
