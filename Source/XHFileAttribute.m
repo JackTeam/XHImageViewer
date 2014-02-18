947934a7d1d317af2030a2e3e9fe3ac011aa65e2
xhzengAIB <xhzengAIB@gmail.com>
2014-02-18 13:37:34 +0000

    implementation XHFileAttribute logic

--- Source/XHFileAttribute.m
+++ Source/XHFileAttribute.m
@@ -10,4 +10,21 @@
 
 @implementation XHFileAttribute
 
+- (id)initWithPath:(NSString *)filePath attributes:(NSDictionary *)attributes {
+    self = [super init];
+    if(self){
+        self.filePath = filePath;
+        self.fileAttributes = attributes;
+    }
+    return self;
+}
+
+- (NSDate *)fileModificationDate {
+    return [_fileAttributes fileModificationDate];
+}
+
+- (NSString *)description {
+    return self.filePath;
+}
+
 @end
