44fd06cd01c59e01a647cf1b7549af03abd35cdd
xhzengAIB <xhzengAIB@gmail.com>
2014-02-18 10:46:11 +0000

    Add category with UIImageView

---
+++ Source/UIImageView+XHURLDownload.m
@@ -0,0 +1,13 @@
+//
+//  UIImageView+XHURLDownload.m
+//  XHImageViewer
+//
+//  Created by 曾 宪华 on 14-2-18.
+//  Copyright (c) 2014年 曾宪华 开发团队(http://iyilunba.com ) 本人QQ:543413507 本人QQ群（142557668）. All rights reserved.
+//
+
+#import "UIImageView+XHURLDownload.h"
+
+@implementation UIImageView (XHURLDownload)
+
+@end
