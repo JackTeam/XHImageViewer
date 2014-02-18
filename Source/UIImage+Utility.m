46c6cca16115882dc1debdccb39d1ee076110427
xhzengAIB <xhzengAIB@gmail.com>
2014-02-18 13:33:28 +0000

    implementation UIImage (Utility) logic

--- Source/UIImage+Utility.m
+++ Source/UIImage+Utility.m
@@ -10,4 +10,27 @@
 
 @implementation UIImage (Utility)
 
++ (UIImage *)decode:(UIImage *)image {
+    if(image==nil) {  return nil; }
+    
+    UIGraphicsBeginImageContext(image.size);
+    {
+        [image drawAtPoint:CGPointMake(0, 0)];
+        image = UIGraphicsGetImageFromCurrentImageContext();
+    }
+    UIGraphicsEndImageContext();
+    
+    return image;
+}
+
++ (UIImage *)fastImageWithData:(NSData *)data {
+    UIImage *image = [UIImage imageWithData:data];
+    return [self decode:image];
+}
+
++ (UIImage *)fastImageWithContentsOfFile:(NSString *)path {
+    UIImage *image = [[UIImage alloc] initWithContentsOfFile:path];
+    return [self decode:image];
+}
+
 @end
