44896553baccb11febce1015b06e2e2d5fd5c0a7
yuxiang <yuxiang.q@hotmail.com>
2014-08-17 17:06:06 +0800

    add imageViewer:didDismissWithSelectedView:;

--- Project/XHImageViewer/XHViewController.m
+++ Project/XHImageViewer/XHViewController.m
@@ -3,7 +3,8 @@
 //  XHImageViewer
 //
 //  Created by 曾 宪华 on 14-2-17.
-//  Copyright (c) 2014年 曾宪华 开发团队(http://iyilunba.com ) 本人QQ:543413507 本人QQ群（142557668）. All rights reserved.
+//  Copyright (c) 2014年 曾宪华 开发团队(http://iyilunba.com ) 本人QQ:543413507
+//  本人QQ群（142557668）. All rights reserved.
 //
 
 #import "XHViewController.h"
@@ -12,75 +13,89 @@
 #import "URLStoreManager.h"
 
 @interface XHViewController () <XHImageViewerDelegate> {
-    NSMutableArray *_imageViews;
+  NSMutableArray *_imageViews;
 }
 
 @end
 
 @implementation XHViewController
 
-- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
-{
-    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
-    if (self) {
-        // Custom initialization
-    }
-    return self;
+- (id)initWithNibName:(NSString *)nibNameOrNil
+               bundle:(NSBundle *)nibBundleOrNil {
+  self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+  if (self) {
+    // Custom initialization
+  }
+  return self;
 }
 
-- (void)viewDidLoad
-{
-    [super viewDidLoad];
-	// Do any additional setup after loading the view.
-    
-    _imageViews = [NSMutableArray array];
-    [_imageViews addObject:[[UIImageView alloc] initWithFrame:CGRectMake(0, 0, 160, 200)]];
-    [_imageViews addObject:[[UIImageView alloc] initWithFrame:CGRectMake(160, 0, 160, 150)]];
-    [_imageViews addObject:[[UIImageView alloc] initWithFrame:CGRectMake(160, 150, 160, 50)]];
-    [_imageViews addObject:[[UIImageView alloc] initWithFrame:CGRectMake(0, 200, 320, 100)]];
-    
-    for (UIImageView *imageView in _imageViews) {
-        NSInteger index = [_imageViews indexOfObject:imageView];
-        UITapGestureRecognizer *gesture = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(tapHandle:)];
-        imageView.userInteractionEnabled = YES;
-        imageView.contentMode = UIViewContentModeScaleAspectFill;
-        imageView.clipsToBounds = YES;
-        [imageView addGestureRecognizer:gesture];
-        
-        if (index) {
-            [imageView loadWithURL:[URLStoreManager getUrlWithIndex:index] placeholer:[UIImage imageNamed:@"placeholder.jpeg"] showActivityIndicatorView:YES];
-        } else {
-            imageView.image = [UIImage imageNamed:@"screenshot.png"];
-        }
-        
-        
-        [self.view addSubview:imageView];
+- (void)viewDidLoad {
+  [super viewDidLoad];
+  // Do any additional setup after loading the view.
+
+  _imageViews = [NSMutableArray array];
+  [_imageViews
+      addObject:[[UIImageView alloc] initWithFrame:CGRectMake(0, 0, 160, 200)]];
+  [_imageViews addObject:[[UIImageView alloc]
+                             initWithFrame:CGRectMake(160, 0, 160, 150)]];
+  [_imageViews addObject:[[UIImageView alloc]
+                             initWithFrame:CGRectMake(160, 150, 160, 50)]];
+  [_imageViews addObject:[[UIImageView alloc]
+                             initWithFrame:CGRectMake(0, 200, 320, 100)]];
+
+  for (UIImageView *imageView in _imageViews) {
+    NSInteger index = [_imageViews indexOfObject:imageView];
+    UITapGestureRecognizer *gesture =
+        [[UITapGestureRecognizer alloc] initWithTarget:self
+                                                action:@selector(tapHandle:)];
+    imageView.userInteractionEnabled = YES;
+    imageView.contentMode = UIViewContentModeScaleAspectFill;
+    imageView.clipsToBounds = YES;
+    [imageView addGestureRecognizer:gesture];
+
+    if (index) {
+      [imageView loadWithURL:[URLStoreManager getUrlWithIndex:index]
+                         placeholer:[UIImage imageNamed:@"placeholder.jpeg"]
+          showActivityIndicatorView:YES];
+    } else {
+      imageView.image = [UIImage imageNamed:@"screenshot.png"];
     }
+
+    [self.view addSubview:imageView];
+  }
 }
 
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)didReceiveMemoryWarning {
+  [super didReceiveMemoryWarning];
+  // Dispose of any resources that can be recreated.
 }
 
 - (void)tapHandle:(UITapGestureRecognizer *)tap {
-    XHImageViewer *imageViewer = [[XHImageViewer alloc] init];
-    imageViewer.delegate = self;
-    imageViewer.disableTouchDismiss = NO;
-    [imageViewer showWithImageViews:_imageViews selectedView:(UIImageView *)tap.view];
+  XHImageViewer *imageViewer = [[XHImageViewer alloc] init];
+  imageViewer.delegate = self;
+  imageViewer.disableTouchDismiss = NO;
+  [imageViewer showWithImageViews:_imageViews
+                     selectedView:(UIImageView *)tap.view];
 }
 
 #pragma mark - XHImageViewerDelegate
 
-- (void)imageViewer:(XHImageViewer *)imageViewer willDismissWithSelectedView:(UIImageView *)selectedView {
-    NSInteger index = [_imageViews indexOfObject:selectedView];
-    NSLog(@"index : %d", index);
+- (void)imageViewer:(XHImageViewer *)imageViewer
+    willDismissWithSelectedView:(UIImageView *)selectedView {
+  NSInteger index = [_imageViews indexOfObject:selectedView];
+  NSLog(@"willDismiss index : %d", index);
 }
 
-- (void) imageViewer:(XHImageViewer *)imageViewer didChangeToImageView:(UIImageView *)selectedView {
-    NSInteger index = [_imageViews indexOfObject:selectedView];
-    NSLog(@"change to index : %d", index);
+- (void)imageViewer:(XHImageViewer *)imageViewer
+    didDismissWithSelectedView:(UIImageView *)selectedView {
+  NSInteger index = [_imageViews indexOfObject:selectedView];
+  NSLog(@"didDismiss index : %d", index);
+}
+
+- (void)imageViewer:(XHImageViewer *)imageViewer
+    didChangeToImageView:(UIImageView *)selectedView {
+  NSInteger index = [_imageViews indexOfObject:selectedView];
+  NSLog(@"change to index : %d", index);
 }
 
 @end
--- Source/XHImageViewer.h
+++ Source/XHImageViewer.h
@@ -3,7 +3,8 @@
 //  XHImageViewer
 //
 //  Created by 曾 宪华 on 14-2-17.
-//  Copyright (c) 2014年 曾宪华 开发团队(http://iyilunba.com ) 本人QQ:543413507 本人QQ群（142557668）. All rights reserved.
+//  Copyright (c) 2014年 曾宪华 开发团队(http://iyilunba.com ) 本人QQ:543413507
+//  本人QQ群（142557668）. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
@@ -13,20 +14,25 @@
 @protocol XHImageViewerDelegate <NSObject>
 
 @optional
-- (void)imageViewer:(XHImageViewer *)imageViewer willDismissWithSelectedView:(UIImageView *)selectedView;
+- (void)imageViewer:(XHImageViewer *)imageViewer
+    willDismissWithSelectedView:(UIImageView *)selectedView;
+- (void)imageViewer:(XHImageViewer *)imageViewer
+    didDismissWithSelectedView:(UIImageView *)selectedView;
 - (UIView *)customNavigationBarOfImageViewer:(XHImageViewer *)imageViewer;
-- (void)imageViewer:(XHImageViewer *)imageViewer didChangeToImageView:(UIImageView *)selectedView;
+- (void)imageViewer:(XHImageViewer *)imageViewer
+    didChangeToImageView:(UIImageView *)selectedView;
 
 @end
 
 @interface XHImageViewer : UIView
 
-@property (nonatomic, weak) id <XHImageViewerDelegate> delegate;
+@property(nonatomic, weak) id<XHImageViewerDelegate> delegate;
 
-@property (nonatomic, assign) CGFloat backgroundScale;
+@property(nonatomic, assign) CGFloat backgroundScale;
 
-@property (nonatomic, assign) BOOL disableTouchDismiss;
+@property(nonatomic, assign) BOOL disableTouchDismiss;
 
-- (void)showWithImageViews:(NSArray*)views selectedView:(UIImageView *)selectedView;
+- (void)showWithImageViews:(NSArray *)views
+              selectedView:(UIImageView *)selectedView;
 
 @end
