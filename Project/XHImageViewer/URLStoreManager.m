c0e93de89b14e589225d80f16bfda3614394eec6
Jack <xhzengAIB@gmail.com>
2015-02-09 16:24:22 +0800

    更改数据源

--- Project/XHImageViewer/URLStoreManager.m
+++ Project/XHImageViewer/URLStoreManager.m
@@ -15,10 +15,14 @@
     NSURL *url = nil;
     switch (index) {
         case 0:
+            url = [NSURL URLWithString:@"http://d.hiphotos.baidu.com/image/h%3D200/sign=22e31b3d324e251ffdf7e3f89787c9c2/72f082025aafa40f282c77daa964034f79f019ca.jpg"];
+            break;
         case 2:
-            url = [NSURL URLWithString:@"http://www.pailixiu.com/jack/1969908.jpg"];
+            url = [NSURL URLWithString:@"http://pic1a.nipic.com/2008-11-07/20081179265582_2.jpg"];
             break;
         case 1:
+            url = [NSURL URLWithString:@"http://pic5.nipic.com/20100202/3760162_130224079498_2.jpg"];
+            break;
         case 3:
             url = [NSURL URLWithString:@"http://pic6.nipic.com/20100423/4782353_093056703685_2.jpg"];
             break;
